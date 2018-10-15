150ae2a74a5fa85422b58a844f21bd305478cc0e
SamKirkiles <sam.kirkiles@gmail.com>
2018-10-15 18:43:17 -0400

    Improved comments

--- boundingbox.py
+++ boundingbox.py
@@ -1,23 +1,25 @@
 import numpy as np
 import cv2
 
+#TODO: Config file with camera ip 
 cap = cv2.VideoCapture("http://172.26.151.84/live")
 face_cascade = cv2.CascadeClassifier('/anaconda/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml')
 
 while(True):
-	# Capture frame-by-frame
+	# Capture frames
 	ret, frame = cap.read()
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 	detected = False
- 
+	# Loop through faces and draw bounding box 
 	for (x,y,w,h) in faces:
 		cv2.putText(frame,"Face",(x,y-5),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),2,cv2.LINE_AA)
 		frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 		detected = True
 
+	# Draw left hand text if face recognized
 	if (detected):
 		cv2.putText(frame,"FACE DETECTED",(5,20),cv2.FONT_HERSHEY_PLAIN,1,(255,255,255),2,cv2.LINE_AA)
 	else:
