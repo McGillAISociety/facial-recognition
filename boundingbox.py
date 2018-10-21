7a7b4ea97dc839f69d0d6a5b7f6eb359a947fdb3
SamKirkiles <sam.kirkiles@gmail.com>
2018-10-21 14:39:26 -0400

    Added toggle for extracted face preview in top left corner. Added option to default to built in webcam if no ip is present in settings.yaml.

--- boundingbox.py
+++ boundingbox.py
@@ -1,9 +1,17 @@
 import numpy as np
 import cv2
 
-def start_inference(ip,weight_address):
-	cap = cv2.VideoCapture(ip)
+
+def start_inference(ip,weight_address,frame_preview):
+
+	if (ip == None):
+		cap = cv2.VideoCapture(0)
+	else:
+		cap = cv2.VideoCapture(ip)
+
 	face_cascade = cv2.CascadeClassifier(weight_address)
+	cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('output', 600,600)
 
 	while(True):
 		# Capture frames
@@ -16,7 +24,13 @@ def start_inference(ip,weight_address):
 		# Loop through faces and draw bounding box 
 		for (x,y,w,h) in faces:
 			cv2.putText(frame,"Face",(x,y-5),cv2.FONT_HERSHEY_PLAIN,3,(255,255,255),2,cv2.LINE_AA)
+			
+			if (frame_preview):
+				draw_preview(frame,(x,y,w,h))
+
 			frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+			
+
 			detected = True
 
 		# Draw left hand text if face recognized
@@ -29,11 +43,25 @@ def start_inference(ip,weight_address):
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 		# Display the resulting frame
-		cv2.imshow('frame',frame)
+		cv2.imshow('output',frame)
+
+
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
 	# When everything done, release the capture
 	cap.release()
 	cv2.destroyAllWindows()
+
+# Preprocesses frame
+def preprocess_frame(frame):
+	pass
+
+# Overwrites frame with preview image of bounding box in top left corner
+def draw_preview(frame,box_coords):
+	# Draw preview in left corner
+	x,y,w,h = box_coords
+	preview = cv2.resize(frame[y:y+h,x:x+w], (0,0), fx=0.5, fy=0.5)
+	ph,pw = preview.shape[:2]
+	frame[0:ph,frame.shape[1]-pw:frame.shape[1]] = preview
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
 
 	IP = None
 	WEIGHT_DIRECTORY = None
+	FRAME_PREVIEW = False
 
 	with open("settings.yaml",'r') as stream:
 		try:
@@ -13,13 +14,21 @@ def main():
 				raise ValueError("IP not listed in settings.yaml")
 			if ('WEIGHT_DIRECTORY' not in settings):
 				raise ValueError("WEIGHT_DIRECTORY not listed in settings.yaml")
+			if ('FRAME_PREVIEW' not in settings):
+				raise ValueError("FRAME_PREVIEW not listed in settings.yaml")
+
 
 			IP = settings['IP']
 			WEIGHT_DIRECTORY = settings['WEIGHT_DIRECTORY']
+			FRAME_PREVIEW = settings['FRAME_PREVIEW']
 
-			print("Starting inference with ip: "  + IP +  " and weight directory: " + WEIGHT_DIRECTORY)
+
+			if (IP == None): ipstr = "BUILT_IN_WEBCAM"
+			else:  ipstr = IP
+
+			print("\nSTARTING INFERENCE\n\n\tVideo Source: "  + ipstr +  "\n\tWeight Directory: " + WEIGHT_DIRECTORY + "\n" + "\tPreview: " + str(FRAME_PREVIEW) + "\n")
 			
-			start_inference(IP,WEIGHT_DIRECTORY);
+			start_inference(IP,WEIGHT_DIRECTORY,FRAME_PREVIEW);
 
 		except yaml.YAMLError as exc:
 			print(exc)
